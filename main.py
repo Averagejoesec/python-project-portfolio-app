c33cb819a8d4afe25603818db7eb5e17448bea71
averagejoesec <root@TBT-averagejoesec>
2024-12-14 16:47:19 +0100

    add description

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.png", width=200)
 
 with col2:
     st.title("Joseph Gonzalez")
@@ -13,3 +13,8 @@ with col2:
     Hi, I'm Joseph! I'm a cybersecurity professional, former educator, and Marine Corps veteran.
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
