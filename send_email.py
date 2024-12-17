566678618378ab7d3d2df1c58fa7c0dde85d5cdd
averagejoesec <root@TBT-averagejoesec>
2024-12-17 13:29:42 +0100

    secrets config

--- send_email.py
+++ send_email.py
@@ -1,14 +1,15 @@
 import smtplib, ssl
+import os
 
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
-    username = ENV_VARIABLE
-    password = ENV_VARIABLE
+    username = os.getenv("USERNAME")
+    password = os.getenv("PASSWORD")
 
-    receiver = ENV_VARIABLE
+    receiver = os.getenv("RECEIVER")
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
